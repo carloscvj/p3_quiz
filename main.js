4169e778527d9b1ed7312debadf221606aafa92a
Carlos Vázquez <carloscvj@gmail.com>
2019-03-05 20:09:27 +0100

    Los comandos en sus case apuntados

--- main.js
+++ main.js
@@ -23,11 +23,39 @@ rl.on('line', (line) => {
             console.log('   add             Añade un nuevo quiz interactivamente');
             console.log('   delete <id>     Borra el quiz indicado');
             console.log('   edit <id>       Edita el quiz indicado');
-            console.log('   test            Prueba el quiz indicado');
+            console.log('   test <id>       Prueba el quiz indicado');
             console.log('   p|play          Comienza el juego');
             console.log('   credits         Créditos');
             console.log('   q|quit          Sale del programa');
-
+            break;
+        case 'q':
+        case 'quit':
+            rl.close();
+            break;
+        case 'list':
+            console.log('   list            Lista de quizers existentes');
+            break;
+        case 'show':
+            console.log('   show <id>       Muestra la pregunta y la respuesta del quiz indicado');
+            break;
+        case 'add':
+            console.log('   add             Añade un nuevo quiz interactivamente');
+            break;
+        case 'delete':
+            console.log('   delete <id>     Borra el quiz indicado');
+            break;
+        case 'edit':
+            console.log('   edit <id>       Edita el quiz indicado');
+            break;
+        case 'test':
+            console.log('   test            Prueba el quiz indicado');
+            break;
+        case 'p':
+        case 'play':
+            console.log('   p|play          Comienza el juego');
+            break;
+        case 'credits':            
+            console.log('   credits         Créditos');
             break;
         default:
             console.log(`Say what? I might have heard '${line.trim()}'`);
